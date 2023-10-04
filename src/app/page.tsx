fea70f7d6c90619bf191e1449a37c2985715200f
Lautaro <laucarrizo332@gmail.com>
2023-10-04 18:40:02 -0300

    Fist commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Titles from "@/components/Titles";
+
+function Home() {
+  return (
+    <div className="relative z-99">
+      <div className="grid place-content-center w-screen h-screen">
+        <Titles />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Titles.tsx
@@ -0,0 +1,14 @@
+function Titles() {
+  return (
+    <div className="w-100 relative text-left flex flex-col align-middle justify-center">
+      <h1 className="text-pink-600 text-5xl fontStyle font-semibold">
+        Saludos, mi nombre es Lautaro Carrizo
+      </h1>
+      <p className="text-pink-900 text-4xl fontStyle font-semibold">
+        Fullstack developer
+      </p>
+    </div>
+  );
+}
+
+export default Titles;
