4cfbc037366316cd56f50365fe4306df1696bd94
Lautaro <laucarrizo332@gmail.com>
2023-10-12 15:06:22 -0300

    add: emoji sun

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,29 +7,38 @@ import Icons from "@/components/Icons";
 import NavBar from "@/components/Navbar";
 import { useState, useEffect } from "react";
 function Home() {
-const [theme, setTheme] = useState("light")
+  const [theme, setTheme] = useState("light");
 
-  
   const handlerChangeTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
 
-useEffect(() => {
-  if (theme === "dark") {
-    document.querySelector("#bg-app")?.classList.add("bg-dark");
-  } else {
-    document.querySelector("#bg-app")?.classList.remove("bg-dark");
-  }
-}, [theme]);
-
+  useEffect(() => {
+    if (theme === "dark") {
+      document.querySelector("#bg-app")?.classList.add("bg-dark");
+    } else {
+      document.querySelector("#bg-app")?.classList.remove("bg-dark");
+    }
+  }, [theme]);
 
   return (
     <div className="h-screen w-full bg-light dark:bg-slate-900" id="bg-app">
       <Cover />
       <div className="w-full h-30">
         <div className="pt-5 pl-5">
-          <button onClick={handlerChangeTheme} className="bg-transparent text-indigo-300 px-4 py-2 rounded transition-transform transition-filter transform hover:scale-125 hover:text-teal-500">
-            Dark Mode. <br/> ⋆⁺₊⋆ ☾⋆⁺₊⋆
+          <button
+            onClick={handlerChangeTheme}
+            className="bg-transparent text-indigo-300 px-4 py-2 rounded transition-transform transition-filter transform hover:scale-125 hover:text-teal-500"
+          >
+            {theme === "light" ? (
+              <span>
+                Dark Mode. <br /> ⋆⁺₊⋆ ☾ ⋆⁺₊⋆
+              </span>
+            ) : (
+              <span>
+                Light Mode. <br /> ⋆⁺₊⋆ ☀ ⋆⁺₊⋆
+              </span>
+            )}
           </button>
         </div>
       </div>
