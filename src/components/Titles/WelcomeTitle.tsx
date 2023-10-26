364011de38616ac8e1031ed63c4cc2c40943fdb7
Lautaro <laucarrizo332@gmail.com>
2023-10-26 10:15:43 -0300

    fix

--- src/components/Titles/WelcomeTitle.tsx
+++ src/components/Titles/WelcomeTitle.tsx
@@ -1,13 +1,15 @@
 import { musicInterface } from "@/types/interface";
-function Welcome({music}:  musicInterface) {
+function Welcome({ music }: musicInterface) {
   const danceTitle = music && "dance";
-    const titleText = "Bienvenido";
+  const titleText = "Bienvenido";
   const titleLetters = titleText.split("");
-  const letterDance = music && "letter-animation"
+  const letterDance = music && "letter-animation";
   return (
     <div className="w-full flex justify-center align-middle h-14 ">
-      <h1 className={`colorTitle text-3xl fontStyle tracking-widest font-semibold ${danceTitle}`}>
-          {titleLetters.map((letter, index) => (
+      <h1
+        className={`colorTitle text-3xl fontStyle tracking-widest font-semibold ${danceTitle}`}
+      >
+        {titleLetters.map((letter, index) => (
           <span key={index} className={`${letterDance}`}>
             {letter}
           </span>
