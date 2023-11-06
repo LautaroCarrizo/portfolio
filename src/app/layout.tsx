a56639b8f7e55d5478cc48ce1f697213cfc37fad
Lautaro <laucarrizo332@gmail.com>
2023-11-06 10:35:13 -0300

    app ready

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,32 +7,31 @@ import NavBar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Portfolio',
+  title: "Portfolio",
   description: "Portfolio",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
- return (
-  <html lang="en">
-    <body className={inter.className}>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Handlee&family=Josefin+Sans:ital,wght@1,300&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <div className="h-screen w-full flex flex-col">
-       
-      {children}
-         <div className="w-full flex bg-nav justify-center">
-          <NavBar />
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Handlee&family=Josefin+Sans:ital,wght@1,300&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <div className="h-screen w-full flex flex-col">
+          {children}
+          <div className="w-full flex bg-nav justify-center">
+            <NavBar />
+          </div>
         </div>
-      </div>
-    </body>
-  </html>
-);
+      </body>
+    </html>
+  );
 }
